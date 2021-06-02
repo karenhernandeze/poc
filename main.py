4f283471abe577e5e4f9e47bf0af05c76f036065
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-06-02 10:37:27 -0600

    fixes

--- main.py
+++ main.py
@@ -72,13 +72,13 @@ async def shutdown():
     await database.disconnect()
 
 
-@app.get("/notes/", response_model=List[Note])
+@app.get("/", response_model=List[Note])
 async def read_notes():
     query = notes.select()
     return await database.fetch_all(query)
 
 
-@app.post("/notes/", response_model=Note)
+@app.post("/", response_model=Note)
 async def create_note(note: NoteIn):
     query = notes.insert().values(text=note.text )
     last_record_id = await database.execute(query)
